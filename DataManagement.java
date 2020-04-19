dca912f92ba140c2070f3b7347ae5bf4c73cd779
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:44:28 -0500

    Update DataManagement.java

--- DataManagement.java
+++ DataManagement.java
@@ -3,10 +3,22 @@ public class DataMangement {
   
   public DataMangement() {}
   
+  /**
+  * searches for the information user wants from the data
+  * @param String userInput input from user
+  */
   public String search(String userInput) {}
   
+  /**
+  * reorders the data by the user's input
+  * @param String userInput input from the user
+  */
   public String getStatistics(String userInput) {}
   
+  /**
+  * put all the data in a specific year
+  * @param String year year to add data to
+  */
   public void createOneYearsData(String year) {}
   
   public File generateFile(File file) {}
