ad7ae33d9a177cb4c172c406e99e2cf175b39809
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:16:01 -0500

    Update YearlyData.java

--- YearlyData.java
+++ YearlyData.java
@@ -1,1 +1,18 @@
-
+public class YearlyData {
+  
+private LinkedList<FarmIDHashTable> allMonths;
+private int totalYearlyWeight;
+private String farmIDWithMostWeight;
+private String farmIDWithLeastWeight;
+  
+  public YearlyData(String year) {}
+  
+  public String generateMonthlyStatistics(String month) {}
+  
+  public void parseFile(File file) {}
+  
+  private String createOneMonthsData(String oneMonth) {}
+  
+  private void createFarmNode(String oneFarmsData) {}
+  
+}
