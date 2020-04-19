ec4c9b86133854992259edc66bc779d149ac31fc
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:31:42 -0500

    Update DataManagement.java

--- DataManagement.java
+++ DataManagement.java
@@ -1,1 +1,17 @@
-
+public class DataMangement {
+   private LinkedList<YearlyData> allYears;
+  
+  public DataMangement() {}
+  
+  public String search(String userInput) {}
+  
+  public String getStatistics(String userInput) {}
+  
+  public void createOneYearsData(String year) {}
+  
+  public File generateFile(File file) {}
+  
+  private String getMonthlyStatistics(String month) {}
+  
+  private String generateYearlyStatistics(String timePeriod) {}
+}
