e32d94c1310acd73a6a0d4e923d2c4302d8f0337
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:51:53 -0500

    Update DataManagement.java

--- DataManagement.java
+++ DataManagement.java
@@ -21,9 +21,23 @@ public class DataMangement {
   */
   public void createOneYearsData(String year) {}
   
+  /**
+  * outputs the statistics into files
+  * @param File file to load statistics to
+  */
   public File generateFile(File file) {}
   
+  /**
+  * Gives the relevant information for a given month, such as
+  * which farm produced the least and most milk
+  * @param String month statistics for that month
+  */
   private String getMonthlyStatistics(String month) {}
   
+  /**
+  * Gives the relevant information for a given year such as
+  * total milk produced
+  * @param String timePeriod
+  */
   private String generateYearlyStatistics(String timePeriod) {}
 }
