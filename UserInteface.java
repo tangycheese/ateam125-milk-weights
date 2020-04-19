dfb7d3c3487ace56fb7e9c5ba2997b7adbd9e7cd
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:07:37 -0500

    Create UserInteface.java

---
+++ UserInteface.java
@@ -0,0 +1,17 @@
+public class UserInterface  () {
+
+public UserInterface() {}
+
+public static void main(String args) {}
+
+private void setStage() {}
+
+private void setScene() {}
+
+private void dataInputString(String userInput) {}
+
+private void dataInputFile(File file) {}
+
+private void dataInputClick(MouseListener listener) {}
+
+private void writeToFile(String fileName) {}
