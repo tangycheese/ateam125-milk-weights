da6182dbd1b867f3c72ae876a9e65e452cf9d479
cmheintz <57310944+cmheintz@users.noreply.github.com>
2020-04-18 19:12:35 -0500

    Update FarmIDHashTable.java

--- FarmIDHashTable.java
+++ FarmIDHashTable.java
@@ -1,4 +1,20 @@
 public class FarmIDHashTable {
+  
+  private int size;
+  private int initialCapacity;
+  private double loadFactor;
+  private final int DAYS_IN_MONTH;
+  private String farmIDWithMostWeightMonthly;
+  private String farmIDWithLeaseWeightMonthly;
+  private HashMap<FarmNode> hashTable;
+  
+  public FarmIDHashTable (String month, int initialCapacity, double loadFactor) {}
+  
+  public void insert(FarmNode farmID) {}
+  
+  public boolean delete(String farmID) {}
+  
+  private FarmNode searchFarmID(String farmID) {}
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
