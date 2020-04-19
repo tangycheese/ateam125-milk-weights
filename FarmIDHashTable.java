b46d95531bebdec4264f1ab0bebd4f9d0530e84c
dredlich1 <61759717+dredlich1@users.noreply.github.com>
2020-04-18 20:35:01 -0500

    Update FarmIDHashTable.java

--- FarmIDHashTable.java
+++ FarmIDHashTable.java
@@ -1,24 +1,124 @@
+import java.util.HashMap;
+
+/**
+ * 
+ * @author Doran Redlich
+ *
+ */
 public class FarmIDHashTable {
-  
-  private int size;
-  private int initialCapacity;
-  private double loadFactor;
-  private final int DAYS_IN_MONTH;
+
+  private int size; // # of farm IDs
+//  private int initialCapacity; //current capacity of HashTable
+//  private double loadFactor; 
+  private final int DAYS_IN_MONTH; // days in this month
   private String farmIDWithMostWeightMonthly;
-  private String farmIDWithLeaseWeightMonthly;
-  private HashMap<FarmNode> hashTable;
+  private String farmIDWithLeastWeightMonthly;
+  private HashMap<String, FarmNode> hashTable; // holds
   
-  public FarmIDHashTable (String month, int initialCapacity, double loadFactor) {}
+  /**
+   * Initializes a new FarmIDHashTable for a given month
+   * @param month - a month represented by an int - January = 1, Feb = 2, etc.
+   */
+  public FarmIDHashTable(int month) {
+    
+    size = 0;
+    //chooses number of days in month based on which month
+    switch(month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        DAYS_IN_MONTH = 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        DAYS_IN_MONTH = 30;
+        break;
+      case 2:
+        DAYS_IN_MONTH = 28;
+        break;
+        default: DAYS_IN_MONTH = 0;
+    }
+    
+    hashTable = new HashMap<String, FarmNode>();
+    
+  }
   
-  public void insert(FarmNode farmID) {}
+  /**
+   * inserts new FarmNode and compares with current farmNodes.
+   * @param farmID - inserts the new FarmNode
+   */
+  public void insert(FarmNode farmID) {
+    hashTable.put(farmID.getFarmID(), farmID);
+    
+    updateFarmRankings(farmID);
+      
+    
+  }
+  /**
+   * 
+   * @param other
+   */
+  private void updateFarmRankings(FarmNode farmID) {
+    
+    if (farmID.compareTo(hashTable.get(farmIDWithMostWeightMonthly))> 0) {
+      farmIDWithMostWeightMonthly = farmID.getFarmID();
+    }
+    
+    else if (farmID.compareTo(hashTable.get(farmIDWithLeastWeightMonthly)) < 0) {
+      farmIDWithLeastWeightMonthly = farmID.getFarmID();
+    }
+  }
   
-  public boolean delete(String farmID) {}
+  /**
+   * @return Returns the number of days in this month
+   */
+  public int getDaysInMonth() {
+    return DAYS_IN_MONTH;
+  }
   
-  private FarmNode searchFarmID(String farmID) {}
-
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+  /**
+   * Deletes the FarmNode if it exists.
+   * @param farmID
+   * @return true if successful, false otherwise.
+   */
+  public boolean deleteFarmID(String farmID) {
+    
+    if(hashTable.get(farmID) == null) {
+      return false;
+    }
+    
+    else 
+      hashTable.remove(farmID);
+      return true;
   }
 
+  /**
+   * Replaces the weight 
+   * @param farmID
+   * @param newDailyWeight
+   * @return true if successful, false otherwise.
+   */
+  public boolean replaceDailyWeight(String farmID, int newDailyWeight, int day) {
+    if(hashTable.get(farmID) == null) return false;
+    else 
+      hashTable.get(farmID).replaceDailyWeight(newDailyWeight, day);
+    updateFarmRankings(hashTable.get(farmID));
+    return true;
+  }
+  
+  /**
+   * @return the FarmNode that is associated with the inputed ID
+   * If null is returned, this farmID does not exist in the hashTable. 
+   */
+  public FarmNode searchForFarm(String farmID) {
+    return hashTable.get(farmID);
+  }
+  
+  
 }
