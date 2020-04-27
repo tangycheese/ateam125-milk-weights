d19b7e23cd5f64ee2b7d74dc81696251ee899893
dredlich1 <61759717+dredlich1@users.noreply.github.com>
2020-04-27 17:53:58 -0500

    Add files via upload

--- YearlyData.java
+++ YearlyData.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -21,19 +22,44 @@ public class YearlyData {
       thisYear = year;
       allMonths = new LinkedList<FarmIDHashTable>();
     }
+    
+    /**
+     * Returns a single farm's data for this whole year.
+     * @param farmID
+     * @return
+     */
+    public StatisticEntity farmReport(String farmID) {
+      return null;
+    }
   
     /**
      * 
      * @param month
      * @return
      */
-    public String generateMonthlyStatistics(String month) {
+    public ArrayList<StatisticEntity> monthlyReport(String month) {
+      return null;
+    }
+    
+    /**
+     * Returns data for all farms over the entire year
+     * @return
+     */
+    public ArrayList<StatisticEntity> annualReport() {
+      return null;
+    }
+    
+    /**
+     * Returns data for all farms over the given data range. 
+     * @return
+     */
+    public ArrayList<StatisticEntity> dataRangeReport(String month1, String day1, String month2, String day2) {
       return null;
     }
 
 
-    public void parseFile(String file){
-        try(BufferedReader reader = new BufferedReader(new FileReader(file));){
+    public void parseFile(String file) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(file));) { // creates new bufferedReader
             FarmIDHashTable oneMonth=null;
             String line=null;
             int lineNo=1;
@@ -59,74 +85,75 @@ public class YearlyData {
         }
     }
 
-    /**
-     * 
-     * @param file
-     */
-    public void parseFile2(String file) {
-      
-      String splitBy = ",";
-      String row = "";
-      BufferedReader reader;
-      String farmID = "";
-      int weight; //the weight of a single day.
-      String[] date; // will parse the date to obtain day, month, year.
-      String[] oneDay; //the parsed row
-      int year;
-      int month;
-      int day;
-      FarmIDHashTable oneMonth;
-      
-      //creates a BufferedReader object to go through the csv file
-      // line by line.
-      try {
-        
-        reader = new BufferedReader(new FileReader(file));
-        row = reader.readLine(); // reads one line of csv file
-        
-        //accesses first row of file - then if it contains "date" skips. 
-        oneDay = row.split(splitBy);
-        if(oneDay[0].contentEquals("date")) {
-            row = reader.readLine();
-        }
-
-        //now attempts to read the second row. 
-        oneDay = row.split(splitBy);
-        date = oneDay[0].split("-"); // array of 'date', where date[0] = the year, date[1] = the month, and date[2] = the day.
-        month = Integer.parseInt(date[1]);
-        day = Integer.parseInt(date[2]);
-            
-        oneMonth = new FarmIDHashTable(month);
-        allMonths.add(oneMonth);
-     
-        farmID = oneDay[1]; // the farm ID to insert
-        weight = Integer.parseInt(oneDay[2]);
-        oneMonth.insert(farmID, day, weight); // now inserts a farmID into the given month. 
-        
-        row = reader.readLine();
-        while (row != null) {
-          oneDay = row.split(splitBy); //splits the row by a comma
-          
-          date = oneDay[0].split("-");  // gets date array
-          day = Integer.parseInt(date[2]); // gets the day to insert as
-          farmID = oneDay[1]; // gets the farmID
-          weight = Integer.parseInt(oneDay[2]); // gets the weight
-          oneMonth.insert(farmID, day, weight); // now inserts using all data. 
-          
-          row = reader.readLine(); // moves onto next line. 
-        }
-        
-      } catch (FileNotFoundException e) {
-        System.out.println(e.getMessage());
-      } catch (IOException e) {
-        System.out.println(e.getMessage()); 
-      } catch (NullPointerException e) {
-        System.out.println(e.getStackTrace());
-      } catch (NumberFormatException e) {
-        System.out.println("Could not parse - in YearlyData: " + e.getMessage());
-      }
-    }
-  
+//    /**
+//     * 
+//     * @param file
+//     */
+//    public void parseFile2(String file) {
+//      
+//      String splitBy = ",";
+//      String row = "";
+//      BufferedReader reader;
+//      String farmID = "";
+//      int weight; //the weight of a single day.
+//      String[] date; // will parse the date to obtain day, month, year.
+//      String[] oneDay; //the parsed row
+//      int year;
+//      int month;
+//      int day;
+//      FarmIDHashTable oneMonth;
+//      
+//      //creates a BufferedReader object to go through the csv file
+//      // line by line.
+//      try {
+//        
+//        reader = new BufferedReader(new FileReader(file));
+//        row = reader.readLine(); // reads one line of csv file
+//        
+//        //accesses first row of file - then if it contains "date" skips. 
+//        oneDay = row.split(splitBy);
+//        if(oneDay[0].contentEquals("date")) {
+//            row = reader.readLine();
+//        }
+//
+//        //now attempts to read the second row. 
+//        oneDay = row.split(splitBy);
+//        date = oneDay[0].split("-"); // array of 'date', where date[0] = the year, date[1] = the month, and date[2] = the day.
+//        month = Integer.parseInt(date[1]);
+//        day = Integer.parseInt(date[2]);
+//            
+//        oneMonth = new FarmIDHashTable(month);
+//        allMonths.add(oneMonth);
+//     
+//        farmID = oneDay[1]; // the farm ID to insert
+//        weight = Integer.parseInt(oneDay[2]);
+//        oneMonth.insert(farmID, day, weight); // now inserts a farmID into the given month. 
+//        
+//        row = reader.readLine();
+//        while (row != null) {
+//          oneDay = row.split(splitBy); //splits the row by a comma
+//          
+//          date = oneDay[0].split("-");  // gets date array
+//          day = Integer.parseInt(date[2]); // gets the day to insert as
+//          farmID = oneDay[1]; // gets the farmID
+//          weight = Integer.parseInt(oneDay[2]); // gets the weight
+//          oneMonth.insert(farmID, day, weight); // now inserts using all data. 
+//          
+//          row = reader.readLine(); // moves onto next line. 
+//        }
+//        
+//      } catch (FileNotFoundException e) {
+//        System.out.println(e.getMessage());
+//      } catch (IOException e) {
+//        System.out.println(e.getMessage()); 
+//      } catch (NullPointerException e) {
+//        System.out.println(e.getStackTrace());
+//      } catch (NumberFormatException e) {
+//        System.out.println("Could not parse - in YearlyData: " + e.getMessage());
+//      }
+//    }
+//  
+    
     /**
      * 
      * @param oneMonth
@@ -230,7 +257,7 @@ public class YearlyData {
                 item.month = Integer.parseInt(dateArr[1].trim());
                 item.day = Integer.parseInt(dateArr[2].trim());
                 return item;
-            }catch (Exception e){
+            } catch (Exception e) {
                 //e.printStackTrace();
                 System.err.println("line "+lineNo +",error line:"+data);
                 return null;
